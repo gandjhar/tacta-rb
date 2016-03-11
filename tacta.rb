6bd23e14b89112f41c96d1d8ad5e24ae5354576c
Andrew Harrison <a.git14@uio9.org>
2016-03-11 13:41:44 +0100

    Search.

--- tacta.rb
+++ tacta.rb
@@ -71,6 +71,19 @@ def action_delete( contacts )
    puts
 end
 
+def action_search( contacts )
+   puts
+   pattern = ask "Search for? "
+   puts
+
+   contacts.each do |contact|
+      if contact[:name] =~ /\b#{pattern}/i
+         show( contact )
+         puts
+      end
+   end
+end
+
 def action_error
    puts
    puts "Sorry, I don't recognize that command."
@@ -81,7 +94,7 @@ loop do
    index( contacts )
 
    puts
-   response = ask "Who would you like to see (n for new, d for delete, q to quit)? "
+   response = ask "Who would you like to see (n for new, d for delete, s for search, q to quit)? "
 
    break if response == "q"
 
@@ -89,6 +102,8 @@ loop do
       action_new( contacts )
    elsif response == "d"
       action_delete( contacts )
+   elsif response == "s"
+      action_search( contacts )
    elsif response =~ /[0-9]+/
       action_show( contacts, response.to_i )
    else
