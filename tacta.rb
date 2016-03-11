c6100e5f120416d21e039cee131e4b561c34a102
Andrew Harrison <a.git14@uio9.org>
2016-03-11 12:51:51 +0100

    Factor actions.

--- tacta.rb
+++ tacta.rb
@@ -36,6 +36,26 @@ def create_new
    contact
 end
 
+def action_new( contacts )
+   contact = create_new
+
+   contacts << contact
+
+   puts
+   puts "New contact created:"
+   puts
+
+   show( contact )
+   puts
+end
+
+def action_show( contacts, i )
+   contact = contacts[i-1]
+
+   puts
+   show( contact )
+   puts
+end
 
 loop do
    index( contacts )
@@ -46,26 +66,9 @@ loop do
    break if response == "q"
 
    if response == "n"
-
-      contact = create_new
-
-      contacts << contact
-
-      puts
-      puts "New contact created:"
-      puts
-
-      show( contact )
-      puts
+      action_new( contacts )
    else
-      i = response.to_i
-
-      contact = contacts[i-1]
-
-      puts
-      show( contact )
-      puts
+      action_show( contacts, response.to_i )
    end
-
 end
 
