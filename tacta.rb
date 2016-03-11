74fe646b2d1223b9175e4a7269babfb8a5081e81
Andrew Harrison <a.git14@uio9.org>
2016-03-11 14:22:23 +0100

    Store contacts in a file.

--- tacta.rb
+++ tacta.rb
@@ -1,10 +1,17 @@
-contacts = []
+require 'json'
 
-contacts << { name: "Thomas Jefferson", phone: "+1 206 310 1369" , email: "tjeff@us.gov"       }
-contacts << { name: "Charles Darwin"  , phone: "+44 20 7123 4567", email: "darles@evolve.org"  }
-contacts << { name: "Nikola Tesla"    , phone: "+385 43 987 3355", email: "nik@inductlabs.com" }
-contacts << { name: "Genghis Khan"    , phone: "+976 2 194 2222" , email: "contact@empire.com" }
-contacts << { name: "Malcom X"        , phone: "+1 310 155 8822" , email: "x@theroost.org"     }
+def read_contacts
+   json = File.read( 'contacts.json' )
+   array = JSON.parse( json, { :symbolize_names => true } )
+end
+
+def write_contacts( contacts )
+   File.open( "contacts.json", "w" ) do |f|
+      json = JSON.pretty_generate( contacts )
+      f.write( json  )
+   end
+end
+
 
 def ask( prompt )
    print prompt
@@ -41,6 +48,8 @@ def action_new( contacts )
 
    contacts << contact
 
+   write_contacts( contacts )
+
    puts
    puts "New contact created:"
    puts
@@ -68,6 +77,8 @@ def action_delete( contacts )
 
    contacts.delete_at( i )
 
+   write_contacts( contacts )
+
    puts
 end
 
@@ -91,6 +102,8 @@ def action_error
 end
 
 loop do
+   contacts = read_contacts
+
    index( contacts )
 
    puts
