095a6f282301848a30e8249c4d1e7b6b5561bc2c
Andrew Harrison <a.git14@uio9.org>
2016-03-12 10:26:24 +0100

    Edit contact.

--- tacta-sn.rb
+++ tacta-sn.rb
@@ -31,9 +31,31 @@ post '/contacts' do
 end
 
 get '/contacts/:i' do
-   i = params[:i].to_i
+   @i = params[:i].to_i
    contacts = read_contacts
-   @contact = contacts[i]
+   @contact = contacts[@i]
    erb :'contacts/show'
 end
+
+get '/contacts/:i/edit' do
+   @i = params[:i].to_i
+
+   contacts = read_contacts
+   @contact = contacts[@i]
+
+   erb :'contacts/edit'
+end
+
+post '/contacts/:i/update' do
+   i = params[:i].to_i
+
+   updated_contact = { name: params[:name], phone: params[:phone], email: params[:email] }
+
+   contacts = read_contacts
+   contacts[i] = updated_contact
+   write_contacts( contacts )
+
+   redirect "/contacts/#{i}"
+end
+
 
