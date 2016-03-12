b9b4fb66ece3dfa242c62ad5dbb654c1d163d2ce
Andrew Harrison <a.git14@uio9.org>
2016-03-12 07:10:44 +0100

    Index Contacts.

--- tacta-sn.rb
+++ tacta-sn.rb
@@ -1,7 +1,14 @@
 require 'sinatra'
+require './contacts_file'
 
 set :port, 4567
 
 get '/' do
    "<h1>Tacta Contact Manager</h1>"
 end
+
+get '/contacts' do
+   @contacts = read_contacts
+   erb :'contacts/index'
+end
+
