254b45191fa473596ac1400c02630e12e1460204
Andrew Harrison <a.git14@uio9.org>
2016-03-12 09:27:53 +0100

    Create contact.

--- tacta-sn.rb
+++ tacta-sn.rb
@@ -18,6 +18,20 @@ get '/contacts/new' do
    erb :'contacts/new'
 end
 
+post '/contacts' do
+   new_contact = { name: params[:name], phone: params[:phone], email: params[:email] }
+
+   contacts = read_contacts
+
+   contacts << new_contact
+
+   write_contacts( contacts )
+
+   i = contacts.length
+
+   redirect "/contacts/#{i}"
+end
+
 get '/contacts/:i' do
    i = params[:i].to_i
    contacts = read_contacts
